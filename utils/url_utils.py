c400f6deb0546880a0493524c27bc8a7e9beecff
Wok <woctezuma@users.noreply.github.com>
2021-01-19 18:09:10 +0100

    Minor reformatting (double quotes instead of single quotes)

--- utils/url_utils.py
+++ utils/url_utils.py
@@ -1,27 +1,27 @@
 def get_heroku_endpoint():
-    return 'render'
+    return "render"
 
 
 def get_query_url(id, num_matches=None):
     heroku_endpoint = get_heroku_endpoint()
-    url = f'/{heroku_endpoint}/{id}'
+    url = f"/{heroku_endpoint}/{id}"
 
     if num_matches is not None:
-        url += f'/{num_matches}'
+        url += f"/{num_matches}"
 
     return url
 
 
 def get_steam_store_url(id):
-    return f'https://store.steampowered.com/app/{id}/'
+    return f"https://store.steampowered.com/app/{id}/"
 
 
 def get_steam_illustration_url(id):
-    return f'https://cdn.cloudflare.steamstatic.com/steam/apps/{id}/library_600x900.jpg'
+    return f"https://cdn.cloudflare.steamstatic.com/steam/apps/{id}/library_600x900.jpg"
 
 
 def get_steamdb_url(id):
-    return f'https://steamdb.info/app/{id}/'
+    return f"https://steamdb.info/app/{id}/"
 
 
 def fill_in_link_url(app, link_to_steam_store=False, num_matches=None):

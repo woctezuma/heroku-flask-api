cfb6331633987b37114a4ddae7d8d31094e6c64d
Wok <woctezuma@users.noreply.github.com>
2021-01-19 16:58:34 +0100

    Add function to fill in a field "link_url" in an app dictionary

--- utils/url_utils.py
+++ utils/url_utils.py
@@ -22,3 +22,19 @@ def get_steam_illustration_url(id):
 
 def get_steamdb_url(id):
     return f'https://steamdb.info/app/{id}/'
+
+
+def fill_in_link_url(app, link_to_steam_store=False, num_matches=None):
+    if link_to_steam_store:
+        link_url = app["steam_store_url"]
+    else:
+        if num_matches is None:
+            # Keep the value of num_matches which was known when creating app["query_url"]
+            link_url = app["query_url"]
+        else:
+            # Override the value of num_matches
+            link_url = get_query_url(app["id"], num_matches)
+
+    app["link_url"] = link_url
+
+    return app

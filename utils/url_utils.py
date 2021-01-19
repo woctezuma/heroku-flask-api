1918a26c1aae31e67b9afc9199b2dda79c4fb6ec
Wok <woctezuma@users.noreply.github.com>
2021-01-19 11:48:51 +0100

    Add utils for creating links to Steam store, SteamDB, or start other queries

---
+++ utils/url_utils.py
@@ -0,0 +1,20 @@
+def get_heroku_name():
+    return 'damp-brushlands-51855'
+
+
+def get_heroku_endpoint():
+    return 'render'
+
+
+def get_app_query_url(id):
+    heroku_name = get_heroku_name()
+    heroku_endpoint = get_heroku_endpoint()
+    return f'https://{heroku_name}.herokuapp.com/{heroku_endpoint}/{id}'
+
+
+def get_steam_store_url(id):
+    return f'https://store.steampowered.com/app/{id}/'
+
+
+def get_steamdb_url(id):
+    return f'https://steamdb.info/app/{id}/'
